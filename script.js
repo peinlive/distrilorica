817a1b7f4d22b068894a7c079dbdec1837eb5b6a
Rafael Ortega <37378229+peinlive@users.noreply.github.com>
2024-10-22 14:35:51 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,84 @@
+const validCredentials = {
+    username: "usuario",
+    password: "c901522492c"
+};
+
+document.getElementById('login-btn').addEventListener('click', function() {
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    const errorMessage = document.getElementById('error-message');
+    
+    if (username === validCredentials.username && password === validCredentials.password) {
+        document.getElementById('login-container').classList.add('hidden');
+        document.getElementById('table-container').classList.remove('hidden');
+        errorMessage.classList.add('hidden');
+    } else {
+        errorMessage.textContent = "Credenciales incorrectas. Intente de nuevo.";
+        errorMessage.classList.remove('hidden');
+    }
+});
+document.getElementById('logout-button').onclick = async function() {
+    await   logout();
+    location.reload()
+};
+
+
+
+
+
+
+
+
+
+
+document.getElementById('search').addEventListener('keyup', function() {
+    const filter = this.value.toLowerCase();
+    const rows = document.querySelectorAll('#data-table tbody tr');
+
+    rows.forEach(row => {
+        row.style.display = row.textContent.toLowerCase().includes(filter) ? '' : 'none';
+    });
+});
+
+function resetSessionTimeout() {
+    // Clear previous timeout
+    clearTimeout(timeoutId);
+    
+    // Set new timeout to log out after 5 minutos (300000 ms)
+    timeoutId = setTimeout(() => {
+        alert('Sesión cerrada por inactividad.');
+        logout();
+    }, 300000);
+
+    // Reset timeout on user activity
+    document.onmousemove = resetSessionTimeout;
+    document.onkeypress = resetSessionTimeout;
+}
+
+function logout() {
+    document.getElementById('login-container').style.display = 'block';
+    document.getElementById('table-container').style.display = 'none';
+    document.getElementById('username').value = '';
+    document.getElementById('password').value = '';
+    clearTimeout(timeoutId);
+}
+
+
+
+ // Mostrar el botón cuando el usuario se desplaza hacia abajo
+ window.onscroll = function() {
+    const btn = document.getElementById("btnVolverArriba");
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        btn.style.display = "block"; // Mostrar botón
+    } else {
+        btn.style.display = "none"; // Ocultar botón
+    }
+};
+
+// Función para volver arriba
+function volverArriba() {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth" // Desplazamiento suave
+    });
+}
